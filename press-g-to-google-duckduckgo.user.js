0c85b26b5c84cf0aee1abd97c4b8c36408d06a10
Gea-Suan Lin <gslin@gslin.org>
2018-12-04 14:01:52 +0800

    Replace %20 to +.

--- press-g-to-google-duckduckgo.user.js
+++ press-g-to-google-duckduckgo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Press "g" to Google (DuckDuckGo)
 // @namespace    https://wiki.gslin.org/wiki/Google
-// @version      0.20180904.0
+// @version      0.20181204.0
 // @description  Press "g" to Google in DuckDuckGo
 // @author       Gea-Suan Lin
 // @match        https://duckduckgo.com/*
@@ -21,7 +21,7 @@
         }
 
         let q = document.getElementById('search_form_input').value;
-        let q_encoded = encodeURIComponent(q);
+        let q_encoded = encodeURIComponent(q).replace(/%20/g, '+');
         let url = 'https://www.google.com/search?q=' + q_encoded;
 
         document.location = url;
